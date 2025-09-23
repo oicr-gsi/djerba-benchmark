c09bdd2f736937de55fcbcc06aa7ec8cf4f2c11e
Hanlin Chen <h2chen@oicr.on.ca>
2025-09-23 12:48:57 -0400

    Updating package path after fork removal

--- src/bin/diff_reports.py
+++ src/bin/diff_reports.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.pop(0) # do not import from script directory
 
-from djerba_benchmark.util.tools import report_equivalence_tester
+from lib.djerba_benchmark.util.tools import report_equivalence_tester
 from djerba.util.logger import logger
 from djerba.util.validator import path_validator
 
